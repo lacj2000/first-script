4545e0e88726e43d46839eb8de00b1eebd25f8a2
Luiz Júnior <41206546+lacj2000@users.noreply.github.com>
2023-08-27 20:43:44 -0300

    Add sprite rotation

--- icon.gd
+++ icon.gd
@@ -1,11 +1,9 @@
 extends Sprite2D
 
+var speed = 400
+var angular_speed = PI
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
